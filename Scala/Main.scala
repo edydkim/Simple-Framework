3ea4c676ab49ee121b6eed61b91ede759d13bcd4
Edy D Kim <me@app-creative.com>
2012-05-02 18:27:35 +0900

    init - main

---
+++ Scala/Main.scala
@@ -0,0 +1,15 @@
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: kim_daiki
+ * Date: 12/04/13
+ * Time: 11:26
+ * To change this template use File | Settings | File Templates.
+ */
+
+object Main {
+
+  def main(args:Array[String]) {
+    for (arg <- args) println(arg)
+  }
+}
